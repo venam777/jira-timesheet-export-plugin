3c0e0a1188f1ddb493978fb00be6e7d5a9c6486b
a.vinakov <a.vinakov@bftcom.com>
2016-08-10 13:15:44 +0300

    Подключил стили и скрипты для конфигурационной страницы и для страницы просмотра задачи + отключил редактирование утвержденых трудозатрат

--- src/main/resources/js/jira-timesheet-export-plugin.js
+++ src/main/resources/js/jira-timesheet-export-plugin.js
@@ -1,10 +1,12 @@
 function setExportMode(isAutoMode) {
-    document.getElementById("exportPeriod").disabled = isAutoMode;
-    document.getElementById("importPeriod").disabled = isAutoMode;
     document.getElementById("startDate").disabled = isAutoMode;
     document.getElementById("endDate").disabled = isAutoMode;
-    document.getElementById("saveButton").value = isAutoMode ? "Сохранить" : "Выполнить";
+    document.getElementById("startAuto").disabled = !isAutoMode;
+    document.getElementById("stopAuto").disabled = !isAutoMode;
+    document.getElementById("executeManualy").disabled = isAutoMode;
+    document.getElementById("startDate").required = !isAutoMode;
+    document.getElementById("endDate").required = !isAutoMode;
 }
-window.onload = function(){
+window.onload = function () {
     setExportMode(true)
 };
---
+++ src/main/resources/js/timesheet-plugin-worklog-script.js
@@ -0,0 +1,55 @@
+/*AJS.$(document).ready (
+    function() {
+        $('#worklog-tabpanel').live('click', function(){
+            $.ajax({
+                url: $(this).attr('href'),
+                success: function(){
+                    $('div[id^="worklog-"]').hover(function () {
+                        if ($(this).find('.panelHeader').text() == 'Не просмотрено') {
+                            $(this).find('.edit-worklog-trigger').css('display', 'none');
+                            $(this).find('.delete-worklog-trigger').css('display', 'none');
+                        }
+                    })
+                }
+            })
+        })
+    }
+);*/
+$(document).ready(function() {
+    disableButtons($('div[id^="worklog-"]'));
+});
+
+oldfunc = window.onload;
+window.onload = function(event) {
+    if (oldfunc) {
+        oldfunc(event);
+    }
+    enableMutationObserver();
+};
+
+
+function enableMutationObserver() {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.addedNodes.length > 0) {
+                for (var i = 0; i < mutation.addedNodes.length; ++i) {
+                    var node = mutation.addedNodes[i];
+                    if ($(node) != undefined && $(node).attr('id') != undefined && $(node).attr('id').search(/worklog-\d{1,}/) != -1) {
+                        disableButtons(node);
+                    }
+                }
+            }
+        });
+    });
+    observer.observe(document.body, {childList: true,  subtree: true});
+}
+
+function disableButtons(node) {
+    $(node).hover(function () {
+        if ($(this).find('.panelHeader').text().search(/^Утверждено/) != -1) {
+            $(this).find('.edit-worklog-trigger').css('display', 'none');
+            $(this).find('.delete-worklog-trigger').css('display', 'none');
+        }
+    });
+}
