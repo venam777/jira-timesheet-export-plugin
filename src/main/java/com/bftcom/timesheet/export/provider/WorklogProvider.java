16d72fbaba25007598214805f2e1c3b8c73a5e0f
a.vinakov <a.vinakov@bftcom.com>
2017-03-20 15:41:50 +0300

    исправил ошибку, когда один и тот же таймшит прилетал несколько раз в день

--- src/main/java/com/bftcom/timesheet/export/provider/WorklogProvider.java
+++ src/main/java/com/bftcom/timesheet/export/provider/WorklogProvider.java
@@ -6,13 +6,11 @@ import com.bftcom.timesheet.export.dto.IssueDTO;
 import com.bftcom.timesheet.export.dto.ProjectDTO;
 import com.bftcom.timesheet.export.dto.WorklogDTO;
 import com.bftcom.timesheet.export.utils.SQLUtils;
+import org.ofbiz.core.entity.GenericDataSourceException;
 import org.ofbiz.core.entity.GenericEntityException;
 import org.ofbiz.core.entity.jdbc.SQLProcessor;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,14 +27,15 @@ public class WorklogProvider {
     }
 
     public Collection<WorklogDTO> getWorklogs(WorklogExportParams params, Long financeProjectFieldId) {
-        Date fromSqlDate = new Date(params.getStartDate().getTime());
-        Date toSqlDate = new Date(params.getEndDate().getTime());
+        Timestamp fromSqlDate = new Timestamp(params.getStartDate().getTime());//new Date(params.getStartDate().getTime());
+        Timestamp toSqlDate = new Timestamp(params.getEndDate().getTime());
         SQLProcessor sqlProcessor = new SQLProcessor("defaultDS");
         List<WorklogDTO> result = new LinkedList<>();
         try {
             String select = "select w.id as worklog_id, w.author as worklog_author, w.worklogbody as worklog_body, w.startdate as worklog_startdate, w.timeworked as worklog_timeworked, i.id as issue_id, i.issuenum as issue_num, i.summary issue_summary, p.id as project_id, p.pkey as project_key, cfo.customvalue as finproject_name ";
             String from = " from worklog w join jiraissue i on w.issueid = i.id join project p on i.project = p.id left join customfieldvalue cf on i.id = cf.issue and cf.customfield = ? left join customfieldoption cfo on cf.stringvalue = cfo.id";
-            String where = " where (w.startdate BETWEEN ? and ? or w.created BETWEEN ? and ?)";
+            String where = " where (w.created BETWEEN ? and ?)";
+            /*w.startdate BETWEEN ? and ? or*/
             if (params.getProjects() != null && params.getProjects().size() > 0) {
                 where += " and p.pkey in " + SQLUtils.collectionToString(params.getProjects(), source -> "'" + source.getKey() + "'");
             }
@@ -47,29 +46,35 @@ public class WorklogProvider {
             PreparedStatement ps = sqlProcessor.getPreparedStatement();
             int index = 1;
             ps.setLong(index++, financeProjectFieldId);
-            ps.setDate(index++, fromSqlDate);
-            ps.setDate(index++, toSqlDate);
-            ps.setDate(index++, fromSqlDate);
-            ps.setDate(index++, toSqlDate);
-            try(ResultSet resultSet = ps.executeQuery()) {
-                while (resultSet.next()) {
-                    String issueKey = resultSet.getString("project_key") + "-" + resultSet.getString("issue_num");
-                    IssueDTO issue = new IssueDTO(resultSet.getInt("issue_id"), issueKey,
-                            resultSet.getString("issue_summary"), resultSet.getString("finproject_name"));
-                    ProjectDTO project = new ProjectDTO(resultSet.getInt("project_id"), resultSet.getString("project_key"));
-                    WorklogDTO worklog = new WorklogDTO(resultSet.getInt("worklog_id"), resultSet.getString("worklog_author"),
-                            resultSet.getString("worklog_body"), resultSet.getDate("worklog_startdate"), resultSet.getLong("worklog_timeworked"), issue, project);
-                    if (params.isIncludeAllStatuses() || worklogDataDao.isWorklogExportable(worklog.getId())) {
-                        result.add(worklog);
-                    }
+//            ps.setDate(index++, new Date(fromSqlDate.getTime()));
+//            ps.setDate(index++, new Date(toSqlDate.getTime()));
+            ps.setTimestamp(index++, fromSqlDate);
+            ps.setTimestamp(index++, toSqlDate);
+            //try {
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                String issueKey = resultSet.getString("project_key") + "-" + resultSet.getString("issue_num");
+                IssueDTO issue = new IssueDTO(resultSet.getInt("issue_id"), issueKey,
+                        resultSet.getString("issue_summary"), resultSet.getString("finproject_name"));
+                ProjectDTO project = new ProjectDTO(resultSet.getInt("project_id"), resultSet.getString("project_key"));
+                WorklogDTO worklog = new WorklogDTO(resultSet.getInt("worklog_id"), resultSet.getString("worklog_author"),
+                        resultSet.getString("worklog_body"), resultSet.getDate("worklog_startdate"), resultSet.getLong("worklog_timeworked"), issue, project);
+                if (params.isIncludeAllStatuses() || worklogDataDao.isWorklogExportable(worklog.getId())) {
+                    result.add(worklog);
                 }
             }
+            //}
         } catch (GenericEntityException | SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                sqlProcessor.close();
+            } catch (GenericDataSourceException e) {
+                e.printStackTrace();
+            }
         }
         return result;
     }
-
 
 
 }
